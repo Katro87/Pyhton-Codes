eaef67da355f44ca168a81cede1ba076e1a118e6
Sufyan Rasheed <109213327+Katro87@users.noreply.github.com>
2025-09-02 23:33:52 -0700

    Update Reverse-word-sentence.py

--- Reverse-word-sentence.py
+++ Reverse-word-sentence.py
@@ -1,5 +1,13 @@
 def reverse(s):
     return s[::-1]
 
+print(reverse("Hello my name is sufyan"))
+def reverse(s):
+    reversed_word = []
+    for word in s.split():
+        reversed_word.append(word[::-1])
+    return reversed_word
+
 
 print(reverse("Hello my name is sufyan"))
+
